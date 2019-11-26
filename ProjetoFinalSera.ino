3ecceba9dc56fa3fb87402a4fef443c6cb1a7a32
franciscotis <franncisco.p@gmail.com>
2019-11-26 17:38:08 -0300

    Update ProjetoFinalSera.ino

--- ProjetoFinalSera.ino
+++ ProjetoFinalSera.ino
@@ -1,3 +1,5 @@
+// Francisco Camille e Thatianne
+
 #include <Thread.h>
 #include <ThreadController.h>
 
@@ -12,7 +14,6 @@ int RPM_Pin = 2;
 float rps;
 float value;
 unsigned long t;
-// Francisco Camille e Thatianne
 
 void setup() {
   
@@ -38,7 +39,7 @@ void setup() {
   cpu.add(&thread2);
   cpu.add(&thread3);
   
-  DDRB |= (1 << PB5); // Set o pino 13 do arduino como saída
+  DDRB |= (1 << PB5); 
 }
 
 
@@ -47,9 +48,6 @@ void ledf(){
     value = Serial.parseFloat();
     thread3.setInterval(float((1000/(value))));
   }  
-  //Led vai ficar piscando em 20 milisegundos
-  //20 milisegundos = 0.02 segundos.
-  // Frequência = 1/T => 50Hz
   Serial.print("LED: ");
   Serial.print(value);
   Serial.println(" Hz");
@@ -57,9 +55,8 @@ void ledf(){
 
 void fan(){
   Serial.print("Motor: ");
-  Serial.print(rps/2); // Valor em1 HZ
-  Serial.println(" Hz"); // Segundo nossos calculos o maximo é 51HZ
-  //Serial.println(rpm*30);
+  Serial.print(rps/2); 
+  Serial.println(" Hz"); 
   rps = 0;
 }
 
@@ -74,13 +71,7 @@ void blinkLed(){
   _delay_ms(0.2);
   delayMicroseconds(0);
   PORTB &= ~(1 <<PB5); // Seta 0 no pino de saída 13
-  //_delay_ms(200);
-  /*
-  digitalWrite(led,HIGH); // HIGH -> equivalente a 1
-  delayMicroseconds(200);
-// delay(miliseconds/2);
-  digitalWrite(led,LOW); // LOW -> equivalente a 0
-  */
+  
 }
 
 void ctt() {
